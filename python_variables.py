a714578a309bba3523e37b26dddf1d0fcd4155b3
GermanC86 <93043761+gercor86@users.noreply.github.com>
2022-06-15 22:07:58 -0300

    Update python_variables.py

--- python_variables.py
+++ python_variables.py
@@ -1,18 +1,18 @@
 
+# AUTOR: German Correa
 
-# AUTOR: GermanC86
-
-# Las variables son unidad de almacenamiento y recuperacion de datos cona valores que pueden cambiar, la cual se identifica con un nombre unico en el codigo del programa.
+# Las variables son unidad de almacenamiento y recuperacion de datos cona valores que pueden cambiar, la cual se identifica con un 
+#  nombre unico en el codigo del programa.
 
 # En python como muchos lenguajes modernos iran automaticamente liberando la memoria con un mecanismo llamado "recolector de basura".
 
-edad = int(input("Ingrese su fecha de nacimiento", "Solo en números)) 
+edad = int(input("Ingrese su fecha de nacimiento", "Solo en números")) 
 
 if edad >= 21:
 	print("Eres mayor de edad")
 
 elif edad == 18:
-	print("Tu edad tiene reconocimientos de derechos pero no de decision propia")
+	print("Tu edad tiene reconocimientos de derechos pero no de decision propia")				
 
 else:
-	print("Eres menor de edad no puedes ingresar").
+	print("Eres menor de edad no puedes ingresar")
